4f4f8b80678da6e30685c07003bd489131810fa3
Mildness Onyekwere <mno5865@rit.edu>
2023-09-07 13:57:30 -0400

    uploading basic tokenizer funcionality from clacc activity: updates needed

---
+++ src/Jott.java
@@ -0,0 +1,52 @@
+public class Jott {
+    public static void main(String[] args) {
+        char[] string = new char[]{'/', '<', ':', ',', '1', '.', 'f'}; //todo get input from file
+        for (int i = 0; i < string.length; i++) {
+            String printString = "";
+            switch (string[i]) {
+                case ',' -> printString = "comma";
+                case ']' -> printString = "rBracket";
+                case '[' -> printString = "lBracket";
+                case '}' -> printString = "rbrace";
+                case '{' -> printString = "lbrace";
+                case ':' -> printString = "colon";
+                case ';' -> printString = "semicolon";
+                case '+', '-', '*', '/' -> printString = "mathOp";
+                case '<', '>' -> printString = "relOp";
+                case '=' -> printString = "assign";
+                case '!' -> printString = "notEquals";
+                case '"' -> printString = "string";
+                case '.' -> {
+                    if (Character.isDigit(string[i + 1])) { //passes = accept state
+                        i++;
+                        printString = digitCheck(string, i);
+                    } else {
+                        printString = "error"; //todo change print to be more specific
+                    }
+                }
+                default -> {
+                    if (Character.isDigit(string[i])) { //passes = accept state
+                        i++;
+                        while (i < string.length && Character.isDigit(string[i])) {
+                            i++;
+                        }
+
+                        if (i < string.length && string[i] == '.') {
+                            printString = digitCheck(string, i);
+                        }
+                    } else if (Character.isAlphabetic(string[i])) {
+                        //todo case if letter, id or keyword
+                    }
+                }
+            }
+            System.out.println(printString);
+        }
+    }
+
+    private static String digitCheck(char[] string, int i) {
+        while (i + 1 < string.length && Character.isDigit(string[i + 1])) {
+            i++;
+        }
+        return "number";
+    }
+}
