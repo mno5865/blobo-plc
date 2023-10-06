830a336b50972eccb74abf32c55d23e8c67a923f
HoldenLalumiere <holdenlalumiere@gmail.com>
2023-10-06 15:14:35 -0400

    Added all missing classes

---
+++ src/nodes/OpNode.java
@@ -0,0 +1,34 @@
+package nodes;
+
+import provided.JottTree;
+
+public class OpNode implements JottTree {
+    @Override
+    public String convertToJott() {
+        String out = "";
+        return out;
+    }
+
+    @Override
+    public String convertToJava(String className) {
+        String out = "";
+        return out;
+    }
+
+    @Override
+    public String convertToC() {
+        String out = "";
+        return out;
+    }
+
+    @Override
+    public String convertToPython() {
+        String out = "";
+        return out;
+    }
+
+    @Override
+    public boolean validateTree() {
+        return false;
+    }
+}
