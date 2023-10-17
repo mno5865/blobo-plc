7bc73fea4ec96980f0638b409fc6cd14e3c26ca7
mpb8984 <71938683+mpb8984@users.noreply.github.com>
2023-10-17 13:13:40 -0400

    while loop node

--- src/nodes/WhileLoopNode.java
+++ src/nodes/WhileLoopNode.java
@@ -1,22 +1,52 @@
 package nodes;
 
+import errors.SyntaxException;
 import provided.JottTree;
 import provided.Token;
+import provided.TokenType;
 
 import java.util.ArrayList;
 
-public class WhileLoopNode implements JottTree {
-    public WhileLoopNode(){
+import static nodes.BasicParsers.parseToken;
 
+public class WhileLoopNode implements JottTree {
+
+    private ExprNode expr;
+    private BodyNode body;
+
+    public WhileLoopNode(ExprNode expr, BodyNode body){
+        this.expr = expr;
+        this.body = body;
     }
 
-    public static WhileLoopNode parseWhileLoopNode(ArrayList<Token> tokens){
-        return new WhileLoopNode();
+    public static WhileLoopNode parseWhileLoopNode(ArrayList<Token> tokens) throws SyntaxException {
+        Token token = tokens.get(0);
+        if(token.getTokenType() != TokenType.ID_KEYWORD)
+        {
+            throw new SyntaxException("Next token must be 'id_keyword'", token.getFilename(), token.getLineNum());
+        } else if(!token.getToken().equals("while"))
+        {
+            throw new SyntaxException("Next token must be an id_keyword of while", token.getFilename(), token.getLineNum());
+        }
+        tokens.remove(0);
+        parseToken(TokenType.L_BRACKET, tokens);
+        ExprNode expr = ExprNode.parseExprNode(tokens);
+        parseToken(TokenType.R_BRACKET, tokens);
+        parseToken(TokenType.L_BRACE, tokens);
+        BodyNode body = BodyNode.parseBodyNode(tokens);
+        parseToken(TokenType.R_BRACE, tokens);
+        return new WhileLoopNode(expr,body);
     }
 
     @Override
     public String convertToJott() {
-        String out = "";
+        String out = "while";
+        out += "[";
+        out += this.expr.convertToJott();
+        out += "]";
+        out += "{";
+        out += this.body.convertToJott();
+        out += "}";
         return out;
     }
 
