f118b312a13adbcc66e90808da5681b132a3d63f
Adrian Burgos <awb8593@rit.edu>
2023-10-18 20:38:17 -0400

    Added case to JottParser for EOF reached before expected

--- src/provided/JottParser.java
+++ src/provided/JottParser.java
@@ -20,12 +20,18 @@ public class JottParser {
      * @return the root of the Jott Parse Tree represented by the tokens.
      *         or null upon an error in parsing.
      */
-    public static JottTree parse(ArrayList<Token> tokens){
+    public static JottTree parse(ArrayList<Token> tokens) {
         JottTree tree = null;
         try {
             tree = ProgramNode.parseProgramNode(tokens);
         } catch (SyntaxException e)  {
             System.err.print(e);
+        } catch (IndexOutOfBoundsException e) {
+            try {
+                throw new SyntaxException("End of file reached before expected.", "", -1);
+            } catch (SyntaxException s) {
+                System.err.print(s);
+            }
         }
         return tree;
     }
