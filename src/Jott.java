ba93a2500e5bffd8016f3bbaaef9a0a3cf1edff2
Gregory Ojiem <79064397+gro3228@users.noreply.github.com>
2023-11-09 20:26:50 -0500

    deleted ugly tabs

--- src/Jott.java
+++ src/Jott.java
@@ -33,7 +33,7 @@ public class Jott {
                 }
                 ArrayList<Token> tokenList = JottTokenizer.tokenize(inputFilename);
                 if (tokenList == null) {
-                    System.err.println("\t\tExpected a list of tokens, but got null");
+                    System.err.println("Expected a list of tokens, but got null");
                     return;
                 }
                 JottTree root = JottParser.parse(tokenList);
